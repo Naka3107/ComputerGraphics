fbdfa32407e6129b41351ed30c6b1718944b558c
Santiago Nakakawa <nakasan3107@gmail.com>
2019-03-06 09:23:15 -0600

    Entrega 3er proyecto.
    Instrucciones de uso. Utilizar las teclas + y - para cambiar de escenas. Dentro de la escena chaikin, utilizar la tecla 'o' para mostrar u ocultar con lineas continuas y presionar '1', '2', '3' y '4' para cambiar iteraciones. '0' Demuestra de regreso la figura sin chaikin. Para desplegar u ocultar la figura original con líneas punteadas, utilizar la tecla 'p'.
    
    Fuentes que se consultaron:
    
    glGenVertexArrays: http://docs.gl/gl4/glGenVertexArrays
    glDeleteVertexArrays: http://docs.gl/gl4/glDeleteVertexArrays
    glBindVertexArray: http://docs.gl/gl4/glBindVertexArray
    glGenBuffers: http://docs.gl/gl4/glGenBuffers
    glDeleteBuffers: http://docs.gl/gl4/glDeleteBuffers
    glBindBuffer: http://docs.gl/gl4/glBindBuffer
    glBufferData: http://docs.gl/gl4/glBufferData
    glEnableVertexAttribArray: http://docs.gl/gl4/glEnableVertexAttribArray
    glVertexAttribPointer: http://docs.gl/gl4/glVertexAttribPointer
    Concoide de Nicomedes: http://mathworld.wolfram.com/ConchoidofNicomedes.html
    Concoide de Nicomedes: http://xahlee.info/SpecialPlaneCurves_dir/ConchoidOfNicomedes_dir/conchoidOfNicomedes.html

--- Computer Graphics/include/scene_chaikin.h
+++ Computer Graphics/include/scene_chaikin.h
@@ -19,19 +19,45 @@ public:
 
 private:
 
-	std::vector<std::vector<cgmath::vec2>> segments = {
-		{{-0.53f, 0.15f }, {-0.81f, 0.42f }, {-0.91f, 0.73f}},
-		{{-0.91f, 0.73f}, {-0.57f, 0.65f}, {-0.27f, 0.32f}}
+	std::vector<std::vector<cgmath::vec2>> originalSegment = {
+		{{-0.56f, 0.10f }, {-0.79f, 0.35f }, {-0.91f, 0.73f}},
+		{{-0.91f, 0.73f}, {-0.57f, 0.61f}, {-0.27f, 0.32f}},
+		{{-0.78f, 0.37f}, {-0.81f, 0.61f}, {-0.78f, 0.69f}},    // Left Ear 3
+		{{-0.27f, 0.32f}, {-0.003f, 0.39f}, {0.23f, 0.33f}},	// Upper Side Head 4
+		{{0.23f, 0.33f}, {0.67f, 0.55f}, {0.95f, 0.61f}},
+		{{0.95f, 0.61f}, {0.83f, 0.34f}, {0.72f, 0.22f}, {0.52f, 0.09f}},
+		{{0.72f, 0.23f}, {0.80f, 0.39f}, {0.81f, 0.58f}},		// Right Ear 7
+		{{-0.53f, 0.15f}, {-0.56f, 0.11f}, {-0.70f, -0.32f}, {-0.53f, -0.59f}, {-0.56f, -0.70f}},  // Left Side Head 8
+		{{-0.56f, -0.70f}, {0.71f, -0.70f}},   // Down Side Head 9
+		{{0.71f, -0.70f}, {0.69f, -0.44f}, {0.62f, -0.23f}, {0.57f, .03f}, {0.46f, 0.16f}},   // Right Side Head 10
+		{{-0.59f, -0.18f}, {-0.52f, -0.18f}, {-0.45f, -0.23f}, {-0.45f, -0.30f}, {-0.51f, -0.34f}, {-0.59f, -0.34f}, {-0.65f, -0.30f}, {-0.65f, -0.23f}, {-0.59f, -0.18f}},     // Left Cheek 11
+		{{0.35f, -0.19f}, {0.44f, -0.19f}, {0.51f, -0.24f}, {0.51f, -0.31f}, {0.44f, -0.37f}, {0.36f, -0.37f}, {0.29f, -0.32f}, {0.29f, -0.24f}, {0.35f, -0.19f}},		// Right Cheek 12
+		{{-0.40f, 0.005f}, {-0.36f, 0.04f}, {-0.30f, 0.04f}, {-0.26f, 0.01f}, {-0.26f, -0.06f}, {-0.30f, -0.10f}, {-0.36f, -0.10f}, {-0.40f, -0.05f}, {-0.40f, 0.005f}},	// Left Eye 13
+		{{-0.364f, 0.028f}, {-0.335f, 0.028f}, {-0.320f, 0.015f}, {-0.320f, -0.016f}, {-0.335f, -0.030f}, {-0.364f, -0.030f}, {-0.378f, -0.015f}, {-0.378f, 0.012f}, {-0.364f, 0.028f}},	// Left Pupil 14
+		{{0.11f, 0.005f}, {0.15f, 0.04f}, {0.21f, 0.04f}, {0.25f, 0.01f}, {0.25f, -0.06f}, {0.21f, -0.10f}, {0.15f, -0.10f}, {0.11f, -0.05f}, {0.11f, 0.005f}},			// Right Eye 15
+		{{0.146f, 0.028f}, {0.175f, 0.028f}, {0.190f, 0.015f}, {0.190f, -0.016f}, {0.175f, -0.030f}, {0.146f, -0.030f}, {0.132f, -0.015f}, {0.132f, 0.012f}, {0.146f, 0.028f}},		// Right Pupil 16
+		{{-0.14f, -0.16f}, {-0.13f, -0.14f}, {-0.08f, -0.14f}, {-0.03f, -0.15f}, {-0.05f, -0.18f}, {-0.10f, -0.18f}, {-0.14f, -0.16f}},		// Nose 17
+		{{-0.18f, -0.32f}, {-0.09f, -0.29f}, {-0.004f, -0.29f}, {0.05f, -0.33f}, {0.07f, -0.42f}, {0.009f, -0.47f}, {-0.09f, -0.47f}, {-0.15f, -0.41f}, {-0.18f, -0.32f}}		// Mouth 18
 	};
-
-
-	GLuint vao[2];
-	GLuint positionsVBO[2];
-	std::vector<std::vector<cgmath::vec2>> currentSegment;
 	
+	GLuint vao[18];
+	GLuint positionsVBO[18];
+
+	GLuint vao2[18];
+	GLuint positionsVBO2[18];
+
+	int currentSegment;
+	std::vector<std::vector<std::vector<cgmath::vec2>>> segments;
+	std::vector<std::vector<cgmath::vec2>> pointedSegments;
+	float const CONSTANTSPACE = 0.01f;
+	bool pointed = false;
+	bool normal = false;
+
 	void initializeSegment(std::vector<std::vector<cgmath::vec2>>& s);
+	void initializePointedSegment();
 	std::vector<cgmath::vec2> chaikin(std::vector<cgmath::vec2>& v);
 	void drawSegment(std::vector<std::vector<cgmath::vec2>>& s);
-
+	void drawPointedSegment();
+	void pointedLineSegment();
 	
 };
--- Computer Graphics/src/scene_chaikin.cpp
+++ Computer Graphics/src/scene_chaikin.cpp
@@ -2,8 +2,12 @@
 
 void scene_chaikin::init()
 {
-	initializeSegment(segments);
-	currentSegment = segments;
+	segments.push_back(originalSegment);
+	currentSegment = 0;
+	pointedSegments.resize(originalSegment.size());
+	pointedLineSegment();
+	initializeSegment(segments[currentSegment]);
+	initializePointedSegment();
 }
 
 void scene_chaikin::awake()
@@ -19,41 +23,105 @@ void scene_chaikin::sleep()
 void scene_chaikin::mainLoop()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	drawSegment(currentSegment);
+	if (pointed) {
+		drawPointedSegment();
+	}
+	if (normal) {
+		drawSegment(segments[currentSegment]);
+	}
 }
 
 void scene_chaikin::normalKeysDown(unsigned char key)
 {
 	if (key == '0') {
-		initializeSegment(segments);
-		currentSegment = segments;
+		int index = key - '0';
+		initializeSegment(segments[index]);
+		currentSegment = index;
 	}
 	if (key == '1') {
+		int index = key - '0';
 		std::vector<std::vector<cgmath::vec2>> newSegmentVector;
-		for (int i = 0; i < segments.size(); i++) {
-			newSegmentVector.push_back(chaikin(segments[i]));
+		for (int i = 0; i < segments[index-1].size(); i++) {
+			newSegmentVector.push_back(chaikin(segments[index - 1][i]));
 		}
-		initializeSegment(newSegmentVector);
-		currentSegment = newSegmentVector;
+		segments.push_back(newSegmentVector);
+		initializeSegment(segments[index]);
+		currentSegment = index;
 	}
 
 	if (key == '2') {
-		
+		int index = key - '0';
+		std::vector<std::vector<cgmath::vec2>> newSegmentVector;
+		for (int i = 0; i < segments[index - 1].size(); i++) {
+			newSegmentVector.push_back(chaikin(segments[index - 1][i]));
+		}
+		segments.push_back(newSegmentVector);
+		initializeSegment(segments[index]);
+		currentSegment = index;
+	}
+
+	if (key == '3') {
+		int index = key - '0';
+		std::vector<std::vector<cgmath::vec2>> newSegmentVector;
+		for (int i = 0; i < segments[index - 1].size(); i++) {
+			newSegmentVector.push_back(chaikin(segments[index - 1][i]));
+		}
+		segments.push_back(newSegmentVector);
+		initializeSegment(segments[index]);
+		currentSegment = index;
+	}
+
+	if (key == '4') {
+		int index = key - '0';
+		std::vector<std::vector<cgmath::vec2>> newSegmentVector;
+		for (int i = 0; i < segments[index - 1].size(); i++) {
+			newSegmentVector.push_back(chaikin(segments[index - 1][i]));
+		}
+		segments.push_back(newSegmentVector);
+		initializeSegment(segments[index]);
+		currentSegment = index;
+	}
+	if (key == 'p') {
+		pointed = !pointed;
+	}
+	if (key == 'o') {
+		normal = !normal;
 	}
 }
 
 void scene_chaikin::initializeSegment(std::vector<std::vector<cgmath::vec2>>& s)
 {
+	glGenVertexArrays(s.size(), vao);
+	glGenBuffers(s.size(), positionsVBO);
+
 	for (int i = 0; i < s.size(); i++) {
-		glGenVertexArrays(s.size(), &vao[i]);
 
 		glBindVertexArray(vao[i]);
-
-		glGenBuffers(s.size(), &positionsVBO[i]);
 		glBindBuffer(GL_ARRAY_BUFFER, positionsVBO[i]);
 		glBufferData(GL_ARRAY_BUFFER,
 			sizeof(cgmath::vec2) * s[i].size(),
 			s[i].data(),
+			GL_DYNAMIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		glBindVertexArray(0);
+
+	}
+}
+
+void scene_chaikin::initializePointedSegment()
+{
+	glGenVertexArrays(pointedSegments.size(), vao2);
+	glGenBuffers(pointedSegments.size(), positionsVBO2);
+
+	for (int i = 0; i < pointedSegments.size(); i++) {
+
+		glBindVertexArray(vao2[i]);
+		glBindBuffer(GL_ARRAY_BUFFER, positionsVBO2[i]);
+		glBufferData(GL_ARRAY_BUFFER,
+			sizeof(cgmath::vec2) * pointedSegments[i].size(),
+			pointedSegments[i].data(),
 			GL_DYNAMIC_DRAW);
 		glEnableVertexAttribArray(0);
 		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
@@ -77,31 +145,67 @@ void scene_chaikin::drawSegment(std::vector<std::vector<cgmath::vec2>>& s)
 	}
 }
 
+void scene_chaikin::drawPointedSegment()
+{
+	for (int i = 0; i < pointedSegments.size(); i++) {
+		glBindVertexArray(vao2[i]);
+		// Llamada a dibujar.
+		// Tipo de primitiva.
+		// Desde qué vértice va a empezar a dibujar.
+		// Cuántos vértices se van a dibujar.
+		glDrawArrays(GL_LINES, 0, pointedSegments[i].size());
+		// Unbind del vao y todos los atributos.
+		glBindVertexArray(0);
+	}
+}
+
+void scene_chaikin::pointedLineSegment()
+{
+	for (int i = 0; i< originalSegment.size(); i++) {
+		for (int j = 0; j+1 < originalSegment[i].size(); j++) {
+
+			cgmath::vec2 vector = (originalSegment[i][j + 1] - originalSegment[i][j]);
+			float magnitude = vector.magnitude();
+			cgmath::vec2 normalizedVector = cgmath::vec2::normalize(vector);
+			for (float p = CONSTANTSPACE; p < magnitude; p += CONSTANTSPACE) {
+				pointedSegments[i].push_back(originalSegment[i][j] + normalizedVector * p);
+			}
+		}
+	}
+}
+
 
 std::vector<cgmath::vec2> scene_chaikin::chaikin(std::vector<cgmath::vec2>& v)
 { 
 	std::vector<cgmath::vec2> newVector;
 	
-	if (v.front() == v.back()) {
-		v.push_back(v[1]);
-	}
-	else {
-		newVector.push_back(v.front());
-	}
-
-	for (int i = 0; i+1 < v.size(); i++) {
-		newVector.push_back(0.75f*v[i] + 0.25f*v[i+1]);
-		newVector.push_back(0.25f*v[i] + 0.75f*v[i+1]);
+	if (v.size() == 2) {
+		return v;
 	}
 	
-	if (v[1] == v.back()) {
-		v.pop_back();
-	}
 	else {
-		newVector.push_back(v.back());
-	}
 
-	return newVector;
+		if (v.front() == v.back()) {
+			v.push_back(v[1]);
+		}
+		else {
+			newVector.push_back(v.front());
+		}
+
+		for (int i = 0; i + 1 < v.size(); i++) {
+			newVector.push_back(0.75f*v[i] + 0.25f*v[i + 1]);
+			newVector.push_back(0.25f*v[i] + 0.75f*v[i + 1]);
+		}
+
+		if (v[1] == v.back()) {
+			v.pop_back();
+		}
+		else {
+			newVector.push_back(v.back());
+		}
+
+		return newVector;
+	}
 }
 
 
